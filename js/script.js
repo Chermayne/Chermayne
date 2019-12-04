ef3083e66c828c63d6b8d4d6e0fb4e0b4186cb80
Chrmyn <57525003+Chermayne@users.noreply.github.com>
2019-12-04 10:11:16 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -31,9 +31,9 @@ dropdown.addEventListener("click", function(){
 
     if (menu.style.display == "none") {
         menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
+//     } else {
+//         menu.style.display = "none";
+//     }
 });
 
 document.addEventListener("click", function(){
